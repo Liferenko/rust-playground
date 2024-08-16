d3af1e5173d951b4bb781db894004c7b143a9779
Pavel Liferenko <eleven.krsk@gmail.com>
2024-08-16 13:31:45 +0200

    refactor: resolve issue with unknown PointJacobi and GENERATOR + huge rework of code with example from chatGPT

--- zk-proof/src/main.rs
+++ zk-proof/src/main.rs
@@ -1,24 +1,23 @@
-// use json; // TODO find in rs
-// use secrets; // TODO find in rs
-// use core::time;
 use chrono::Utc;
-use crypto_bigint::consts::U256;
+use crypto_bigint::Encoding;
 use core::str;
 use std::fmt::format;
 use std::collections::HashMap;
 use getrandom;
 
-use crypto_bigint::{U256, Encoding};
-use ecdsa::{self, ECDSA_SHA224_OID};
-use ecdsa::elliptic_curve::{self, point}; // TODO find PointJacobi
-use std::hash::Hash;
+use k256::{elliptic_curve::Field, ProjectivePoint, Scalar, U256, sha2::Sha256};
+
+//# TODO: REMOVE BEFORE FLIGHT!!!!!!
+// use crypto_bigint::{U256, Encoding};
+// use crypto_bigint::consts::U256;
+// use ecdsa::{self, ECDSA_SHA224_OID};
+// use ecdsa::elliptic_curve::{self, point, ScalarPrimitive}; // TODO find ProjectivePoint
 use sha256::digest;
 
 
 
-const curve = todo!("ecdsa::p256"); //TODO: shall I use static instead?
-const G = todo!("curve.generator");
-const Q = todo!("curve.order");
+const G = ProjectivePoint::GENERATOR;
+const Q = todo!() // TODO: define which order shoyld be in use here
 
 
 
@@ -27,84 +26,74 @@ fn generate_random_number() -> U256 {
 }
 
 // TODO:
-// - define PointJacobi
+// - define ProjectivePoint
 #[derive(PartialEq)]
 struct DLogProof {
-    fn 
+    t: ProjectivePoint,
+    s: Scalar,
 
-    fn _hash_points(sid: &str, pid: u8, points: [PointJacobi]) -> U256  // TODO replace PointJacobi
-
-    fn prove(sid: &str, pid: u8, x: u128, y: PointJacobi, base_point: PointJacobi = G) -> boolean,
-
-    fn verify(self, sid: &str, pid: u8, y: PointJacobi, base_point: PointJacobi = G) -> boolean,
-
-    fn from_dict(data: HashMap<K, V>),
-    fn to_dict(self) -> HashMap<K, V>,
-    fn to_str(self) -> str
-
+    // Sidenote: seems like these pre-def methods are not required. Need to doublecheck
+    // fn new(t: ProjectivePoint, s: Scalar) -> Self,
+    //
+    // fn _hash_points(sid: &str, pid: u8, points: [ProjectivePoint]) -> U256  // TODO replace ProjectivePoint
+    //
+    // fn prove(sid: &str, pid: u8, x: u128, y: ProjectivePoint, base_point: ProjectivePoint = G) -> boolean,
+    //
+    // fn verify(self, sid: &str, pid: u8, y: ProjectivePoint, base_point: ProjectivePoint = G) -> boolean,
+    //
+    // fn from_dict(data: HashMap<K, V>),
+    // fn to_dict(self) -> HashMap<K, V>,
+    // fn to_str(self) -> str
+    //
 }
 
 impl DLogProof {
-    // TODO: find out how to impl __init__
-    fn new(self, t: PointJacobi, s: u8) {
-        self.t = t;
-        self.s = s;
-        // TODO looks bad. Need to double-check those self.* entities
+    fn new(t: ProjectivePoint, s: Scalar) -> Self {
+        DLogProof{t, s};
     }
     
-    fn _hash_points(self, sid: &str, pid: u8, points: Vec<PointJacobi>) {
+    fn _hash_points(self, sid: &str, pid: u8, points: [ProjectivePoint]) -> Scalar {
         let string_field = ""; // TODO: StringField
-        let bigint_field = U256::new();
-        let pount_field = ECDSA_SHA224_OID; // TODO:
+        let bigint_field = U256::new(32);
+        let point_field = ProjectivePoint; // TODO:
 
-        let mut digest_data = String::new();
-        digest_data.push_str(string_field.to_bytes(sid)); // TODO: replace .to_bytes(sid)
-        digest_data.push(bigint_field.to_bytes(pid)); // TODO: replace .to_bytes(sid) && .push or
-        // .push_str?
+        let mut digest_data = Sha256::new();
+        digest_data.update(string_field.to_bytes(sid));
+        digest_data.update(bigint_field.to_bytes(&pid));
         
         for point in points {
-            digest_data.push(pount_field.to_bytes(point));
+            digest_data.update(point_field.to_bytes(point));
         }
 
         let digest = sha256::digest(digest_data);
 
-        U256::from(digest);
+        U256::from_be_bytes(digest);
     }
 
 
-    fn prove(self, sid: &str, pid: u8, x: u128, y: PointJacobi, base_point: PointJacobi = G) {
+    fn prove(self, sid: &str, pid: u8, x: u128, y: ProjectivePoint) -> DLogProof { // TODO: G?
         let r = generate_random_number();
-        let t = r * base_point;
-        let c = DLogProof._hash_points(sid, pid, [base_point, y, t]);
-        let s = (r + c * x) % DLogProof.q;
+        let t = r * G;
+        let c = self._hash_points(sid, pid, [G, y, t]);
+        let s = (r + c * x) % Q;
 
-        DLogProof(t, s);
-        todo!()
+        DLogProof::new(t, s)
     }
 
-    fn verify(self, sid: &str, pid: u8, y: PointJacobi, base_point: PointJacobi = G) {
-        let c = self._hash_points(sid, pid, [base_point, y, t]);
-        let lhs = self.s * base_point;
+    fn verify(self, sid: &str, pid: u8, y: ProjectivePoint) -> bool {
+        let c = self._hash_points(sid, pid, [G, y, self.t]);
+        let lhs = self.s * G;
         let rhs = self.t + (y * c);
-        lhs == rhs;
-        todo!()
+
+        lhs == rhs
     }
 
-    fn to_dict(self) {
-        HashMap{
-            "t": todo!(),
-            "s": todo!()
-        }
-        todo!()
-    }
-
-    fn to_str(self) {
-        json::dumps(self.to_dict());
-        todo!()
+    fn to_str(&self) -> String {
+        serde_json::to_string(self).unwrap()
     }
     
     fn from_dict(data: HashMap<K, V>) {
-        todo!()
+        todo!()   
     }
 }
 
